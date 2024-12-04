5e101c0990c6653c189e8908643930c5a718378a
good593 <goodwon593@gmail.com>
2024-12-04 14:57:19 +0900

    일부 코드 수정

--- sns/slack/lambda/alarm/app.py
+++ sns/slack/lambda/alarm/app.py
@@ -5,8 +5,8 @@ logger.setLevel(logging.INFO)
 import warnings
 warnings.filterwarnings(action='ignore')
 
-from sns_slack import slack_alarm
-from constant import SLACK_CHANNELS, SERVICE_TYPE
+from common.sns_slack import slack_alarm
+from common.constant import SLACK_CHANNELS, SERVICE_TYPE
 
 def lambda_handler(event:dict, context:str) -> None:
   logging.info("lambda_handler!!")
