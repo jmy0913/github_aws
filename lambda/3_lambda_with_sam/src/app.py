fc7f3ca0a2af3b14734de31efcaef7b120569873
good593 <goodwon593@gmail.com>
2025-04-15 20:09:29 +0900

    작성중

--- lambda/3_lambda_with_sam/src/app.py
+++ lambda/3_lambda_with_sam/src/app.py
@@ -13,7 +13,7 @@ def lambda_handler(event:dict, context:str) -> None:
   logging.info("lambda_handler START")
   path_csv = mk_path_csv_of_s3("lambda_test")
   path_csv = "raw/"+path_csv
-  bucket_name = "생성한 bucket명"
+  bucket_name = "lambda-good593"
 
   upload_csv_to_s3(data=pd.DataFrame([event])
                     , bucket=bucket_name, path=path_csv)
